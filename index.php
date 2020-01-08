7dfd8d357db8c64d6b6a8b1ddea6b09c0abefd67
FLEMAL <55393709+Rom-1-code@users.noreply.github.com>
2020-01-08 17:46:37 +0100

    delete + index/css

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="home.png" />
+    <title>Acceuil</title>
+</head>
+
+<body>
+    <h1>
+        <?php echo "Bienvenue"; ?>
+    </h1>
+    <h2>
+        Liste de mes exercices
+    </h2>
+    <nav>
+        <ul>
+            <li class="deroulant"><a href="#">Exercice PHP &ensp;</a>
+                <ul class="sous">
+                    <li><a href="/Exo Partie 3 PHP/exercice1/Exercice 1.php">Exercice 1</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice2/Exercice 2.php">Exercice 2</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice3/Exercice 3.php">Exercice 3</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice4/Exercice 4.php">Exercice 4</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice5/Exercice 5.php">Exercice 5</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice6/Exercice 6.php">Exercice 6</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice7/Exercice 7.php">Exercice 7</a></li>
+                    <li><a href="/Exo Partie 3 PHP/exercice8/Exercice 8.php">Exercice 8</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
